840f75d437967deb17e37f0f14c0b2339908fe52
Priscila Ravanelli <priscila.andreani@b2wdigital.com>
2022-01-05 22:32:51 -0300

    app: remove unused routes

--- src/App.js
+++ src/App.js
@@ -12,30 +12,10 @@ import Footer1 from './component/layout/Footer1';
 
 function App() {
   return (
-    <Routes>
+    <>
       <Navbar />
-      <Container customClass="min-height">
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/sobre">
-          <Sobre />
-        </Route>
-        <Route exact path="/acoes">
-          <Acoes />
-        </Route>
-        <Route exact path="/doacoes">
-          <Doacoes />
-        </Route>
-        <Route exact path="/voluntariado">
-          <Voluntariado />
-        </Route>
-        <Route exact path="/loja">
-          <Loja />
-        </Route>
-      </Container>
-      <Footer1 />
-    </Routes>
+      <Home />
+    </>
   );
 }
 
