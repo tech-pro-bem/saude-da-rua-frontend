8f77e26c0caef75ee95968938275ae8181dee4fc
Priscila Ravanelli <priscila.andreani@b2wdigital.com>
2022-01-05 22:28:43 -0300

    chore: remove rbx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Home from './component/pages/Home';
 import Acoes from './component/pages/Acoes';
 import Doacoes from './component/pages/Doacoes';
@@ -10,35 +10,30 @@ import Container from './component/layout/Container';
 import Navbar from './component/layout/Navbar1';
 import Footer1 from './component/layout/Footer1';
 
-//teste Bulma
-import 'rbx/index.css';
-
 function App() {
   return (
     <Routes>
       <Navbar />
-      <Switch>
-        <Container customClass="min-height">
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/sobre">
-            <Sobre />
-          </Route>
-          <Route exact path="/acoes">
-            <Acoes />
-          </Route>
-          <Route exact path="/doacoes">
-            <Doacoes />
-          </Route>
-          <Route exact path="/voluntariado">
-            <Voluntariado />
-          </Route>
-          <Route exact path="/loja">
-            <Loja />
-          </Route>
-        </Container>
-      </Switch>
+      <Container customClass="min-height">
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/sobre">
+          <Sobre />
+        </Route>
+        <Route exact path="/acoes">
+          <Acoes />
+        </Route>
+        <Route exact path="/doacoes">
+          <Doacoes />
+        </Route>
+        <Route exact path="/voluntariado">
+          <Voluntariado />
+        </Route>
+        <Route exact path="/loja">
+          <Loja />
+        </Route>
+      </Container>
       <Footer1 />
     </Routes>
   );
--- src/component/layout/Navbar1.js
+++ src/component/layout/Navbar1.js
@@ -3,48 +3,8 @@ import { Link } from 'react-router-dom';
 /*import styles from './Navbar.module.css'*/
 /*import logo from '../../img/logo.png'*/
 
-
-import { Navbar} from 'rbx';
-
 function Navbar1() {
-
-    return (
-        <Navbar class = "has-background-black"> 
-        
-            <Navbar.Brand >                                    
-                <Navbar.Burger />
-            </Navbar.Brand>
-            <Navbar.Menu >
-                <Navbar.Segment align="end">               
-                    <Link to="/">
-                        <Navbar.Item >Saúde da Rua</Navbar.Item>
-                    </Link>
-                    <Link to="/acoes">
-                        <Navbar.Item >Ações</Navbar.Item>
-                    </Link>                    
-                    <Link to="/sobre">
-                        <Navbar.Item >Sobre</Navbar.Item>
-                    </Link>                                        
-                    <Link to="/voluntariado">
-                        <Navbar.Item >Seja Voluntário</Navbar.Item>
-                    </Link>
-                    <Link to="/loja">
-                        <Navbar.Item >Loja</Navbar.Item>
-                    </Link>
-                </Navbar.Segment>
-                <Navbar.Segment align="end">
-                    <Navbar.Item>
-                    </Navbar.Item>
-                </Navbar.Segment>
-            </Navbar.Menu>
-        </Navbar>
-    )
+  return <div></div>;
 }
 
 export default Navbar1;
-
-
-/*navbar is-primary    has-background-black */
-                    
-
-             
