aad5fe7aed811d5ee595a9fd5230c46a92a5e82f
Priscila Ravanelli <priscila.andreani@b2wdigital.com>
2022-01-05 22:35:30 -0300

    app: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Home from './component/pages/Home';
-import Acoes from './component/pages/Acoes';
-import Doacoes from './component/pages/Doacoes';
-import Loja from './component/pages/Loja';
-import Sobre from './component/pages/Sobre';
-import Voluntariado from './component/pages/Voluntariado';
 
-import Container from './component/layout/Container';
 import Navbar from './component/layout/Navbar1';
-import Footer1 from './component/layout/Footer1';
 
 function App() {
   return (
