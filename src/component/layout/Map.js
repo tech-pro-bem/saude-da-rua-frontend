a9e41f6b73eba1f37bd6492f5e9a4a5f73cdfd84
leandrosax10 <leandrosantosti730@gmail.com>
2022-01-14 20:30:20 -0300

    Alter page map

--- src/component/layout/Map.js
+++ src/component/layout/Map.js
@@ -1,9 +1,13 @@
 import Mapa from './../../img/mapa.png';
 
-import MapImag from './styles';
+import { MapImag } from './styles';
 
 function Map() {
-  <MapImag src={Mapa} alt="Foto do Mapa" />;
+  return (
+    <>
+      <MapImag src={Mapa} alt="Foto do Mapa" />;
+    </>
+  );
 }
 
 export default Map;
