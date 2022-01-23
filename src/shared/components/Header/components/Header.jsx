a9ce3885026ac5c584666ca6dbd5112ca0c84f4a
Maridh <maribranding@gmail.com>
2022-01-22 22:44:15 -0300

    Adding Margin and Padding in Header

--- src/shared/components/Header/components/Header.jsx
+++ src/shared/components/Header/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from '../../../../assets/Logo_saude_da_rua.png';
+import Padding from './Header.style';
+
 const Header = () => {
   return (
-    <div>
-      <div className="header">
-        <img src={logo} alt="Logo Saúde da Rua" className="logo" />
+    <Padding>
+      <div>
+        <div className="header">
+          <img src={logo} alt="Logo Saúde da Rua" className="logo" />
+        </div>
       </div>
-    </div>
+    </Padding>
   );
 };
 
---
+++ src/shared/components/Header/components/Header.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Padding = styled.div`
+  padding-left: 8.375rem;
+  margin-top: 1.0625rem;
+`;
+export default Padding;
