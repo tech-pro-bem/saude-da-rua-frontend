0dac3da67377f9c4e0b9c171ceec842e8203dd79
Maridh <maribranding@gmail.com>
2022-02-04 17:02:07 -0300

    List insertion

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,8 @@ import imag1 from './DSC_0182.png';
 import imag2 from './H21.png';
 import imag3 from './H61.png';
 import partnersLogo from './partners.png';
+import social1 from './WhatsApp.svg';
+import social2 from './Email.svg';
+import social3 from './Instagram.svg';
 
-export { imag1, imag2, imag3, partnersLogo };
+export { imag1, imag2, imag3, partnersLogo, social1, social2, social3 };
--- src/shared/components/Footer/components/Footer.style.js
+++ src/shared/components/Footer/components/Footer.style.js
@@ -1,16 +1,56 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  * {
+    color: ${(props) => props.theme.colors.primary_light};
+  }
+
+  .ft-main {
+    display: flex;
+    margin-bottom: 7.5rem;
+  }
+
   hr {
-    margin-bottom: 60px;
-    border: #e9e9e9 solid 1px;
-    width: 1170px;
+    margin-bottom: 3.75rem;
+    border: #e9e9e9 solid 0.0625rem;
+    width: 73.12rem;
     margin: 0 auto;
   }
 
-  .logoFooter {
-    margin-top: 60px;
-    margin-left: 400px;
-    margin-bottom: 138px;
+  .ft-title,
+  .ft-title3 {
+    margin-top: 3.75rem;
+    margin-left: 8.5rem;
+    margin-bottom: 2.31rem;
+  }
+
+  .ft-title3 {
+    margin-left: 8.5rem;
+  }
+
+  .ft-legal-list {
+    display: flex;
+    list-style-type: none;
+    justify-content: center;
+  }
+
+  .ft-legal-list > li {
+    margin-right: 2.5rem;
+    margin-bottom: 1.56rem;
+  }
+
+  .locations,
+  .midia,
+  p {
+    margin-left: 8.62rem;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  .logo {
+    margin-top: 0.62rem;
+    margin-right: 1.25rem;
   }
 `;
