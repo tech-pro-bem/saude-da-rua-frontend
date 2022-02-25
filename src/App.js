e7f9a9e819a3111700ecf4c16c065abdd426c459
Lucas <lucas-m@outlook.com>
2022-02-25 00:00:05 -0300

    move routes from app to a dedicated path
    Move code for routes in the App code into a proper component on a dedicated folder for routes. The binding between pages and routes are in the route component instead of the app component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { Home } from './pages';
-import { Layout } from './shared/components';
+import AppRoutes from './routes/AppRoutes';
 import GlobalStyle from './styles/GlobalStyles';
 import theme from './styles/theme';
 
@@ -11,15 +10,7 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <BrowserRouter>
-          {/*
-          OBS: Elementos que ocupam mais de 1140px de largura devem estar FORA da estrutura
-       */}
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="doacao" element={() => <div></div>} />
-            </Route>
-          </Routes>
+          <AppRoutes />
         </BrowserRouter>
       </ThemeProvider>
     </>
