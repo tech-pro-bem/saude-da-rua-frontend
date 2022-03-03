d6e55511d51f896f5b6149d74bf3008272f7fbee
Lucas <lucas-m@outlook.com>
2022-03-02 21:27:13 -0300

    add qr codes images

---
+++ src/assets/qr-codes/index.js
@@ -0,0 +1,9 @@
+import BRL15 from './BRL15.svg';
+import BRL30 from './BRL30.svg';
+import BRL50 from './BRL50.svg';
+import BRL100 from './BRL100.svg';
+import BRL200 from './BRL200.svg';
+
+const qrCodes = { BRL15, BRL30, BRL50, BRL100, BRL200 };
+
+export default qrCodes;
