3b74274c71a7660064eb247f557d401632667c20
Lucas <lucas-m@outlook.com>
2022-03-03 18:55:23 -0300

    add exportation file for svg assets

---
+++ src/assets/svg/index.js
@@ -0,0 +1,5 @@
+import assistanceIcon from './assistance.svg';
+import healthcareIcon from './healthcare.svg';
+import supportIcon from './support.svg';
+
+export { assistanceIcon, healthcareIcon, supportIcon };
