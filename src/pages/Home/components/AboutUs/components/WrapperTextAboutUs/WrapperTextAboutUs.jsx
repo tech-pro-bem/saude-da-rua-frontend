0ed972e59210f975a794223c90ef4b73449d29ea
priscila andreani <priscilaandreani@gmail.com>
2022-03-15 14:48:13 -0300

    home: aboutUs: refact component and add clean code

--- src/pages/Home/components/AboutUs/components/WrapperTextAboutUs/WrapperTextAboutUs.jsx
+++ src/pages/Home/components/AboutUs/components/WrapperTextAboutUs/WrapperTextAboutUs.jsx
@@ -1,30 +1,38 @@
-import { AboutTitle, AboutText } from './WrapperTextAboutUs.style';
+import {
+  QuemSomosTitle,
+  AboutText,
+  Container,
+} from './WrapperTextAboutUs.style';
 
-const WrapperTextAboutUs = () => {
+///** TODO:
+// - refatorar function para function component
+// - manter padrão da linguagem (ou PT ou EN)
+// - por que foi utilizado um Fragment aqui?
+// A utilização de uma div estilizada facilitaria a organização de estilo
+// - rodar o console log para ver os erros que indicam aqui
+// - um p não pode existir dentro de outro p (QuemSomosTitle)
+
+function WrapperTextAboutUs() {
   return (
-    <div>
-      <AboutTitle>
-        <h2>Quem somos</h2>
-      </AboutTitle>
-      <AboutText>
-        <p className="big">
-          O Saúde da Rua nasceu em julho de 2020, em meio à pandemia do
-          Covid-19, com a missão de{' '}
-          <strong> levar atendimento médico humanizado </strong>e recorrente às
-          camadas mais vulneráveis da sociedade.
-        </p>
+    <Container>
+      <h2>Quem somos</h2>
 
-        <p className="big">
-          Desde então, com o trabalho de cerca de
-          <strong> 500 voluntários </strong> realizamos diversas ações, com o
-          objetivo de promover um
-          <strong> serviço de alta qualidade </strong>, de forma individualizada
-          e única, direcionado as
-          <strong> reais necessidades do paciente</strong>.
-        </p>
-      </AboutText>
-    </div>
+      <p className="big">
+        O Saúde da Rua nasceu em julho de 2020, em meio à pandemia do Covid-19,
+        com a missão de <strong> levar atendimento médico humanizado </strong>e
+        recorrente às camadas mais vulneráveis da sociedade.
+      </p>
+
+      <p className="big">
+        Desde então, com o trabalho de cerca de
+        <strong> 500 voluntários </strong> realizamos diversas ações, com o
+        objetivo de promover um
+        <strong> serviço de alta qualidade </strong>, de forma individualizada e
+        única, direcionado as
+        <strong> reais necessidades do paciente</strong>.
+      </p>
+    </Container>
   );
-};
+}
 
 export default WrapperTextAboutUs;
