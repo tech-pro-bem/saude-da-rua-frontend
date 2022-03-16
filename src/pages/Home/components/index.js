40700ff7e0916ee62be7438f0167f4193374a689
Maridh <maribranding@gmail.com>
2022-03-16 20:32:16 -0300

    Feat: Add banner
    Signed-off-by: Maridh <maribranding@gmail.com>

--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -1,5 +1,6 @@
 import AboutUs from './AboutUs/AboutUs';
 import Partners from './Partners/Partners';
 import WhereAreWe from './WhereAreWe/WhereAreWe';
+import Banner from './Banner/Banner';
 
-export { AboutUs, Partners, WhereAreWe };
+export { AboutUs, Partners, WhereAreWe, Banner };
