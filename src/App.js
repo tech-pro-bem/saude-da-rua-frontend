e059a65ce368e816d5e5ec857ebc29d93ef752fc
Lucas <lucas-m@outlook.com>
2022-03-16 22:16:46 -0300

    feat: add react-query Query Provider to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import AppRoutes from './routes/AppRoutes';
 import GlobalStyle from './styles/GlobalStyles';
 import theme from './styles/theme';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 };
 
