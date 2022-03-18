8e1c542147d3065048acd7ee695b5d3a1ab001e1
Lucas <lucas-m@outlook.com>
2022-03-18 01:34:35 -0300

    refact: change fetching implementation from react state/useEffect to react-query

--- src/pages/Home/components/Instagram/Instagram.jsx
+++ src/pages/Home/components/Instagram/Instagram.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Highlight, OutlinedImage } from '../../../../shared/components';
 import { InstagramClient } from '../../../../shared/services';
 import {
@@ -9,27 +9,21 @@ import {
   IgPictureCard,
 } from './Instagram.style';
 
+const IG_USER_DEFAULT = { username: 'saudedarua' };
+
 const Instagram = () => {
-  // set default username initially, but update as soon as possible, since users can change their @usernames
-  const [igLink, setIgLink] = useState('https://www.instagram.com/saudedarua');
-  const [igPictures, setIgPictures] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // use default username initially, but update as soon as possible, since users can change their @usernames
+  const { data: user } = useQuery(
+    'ig/me',
+    () => InstagramClient.getSelfUser(),
+    { placeholderData: IG_USER_DEFAULT }
+  );
 
-  // TODO: create custom hook to handle fecthing general logic
-  useEffect(() => {
-    const fetchIgData = async () => {
-      const fields = ['id', 'media_url', 'caption', 'permalink'];
-      const lastPictures = await InstagramClient.getLastPictures(3, fields);
-      setIgPictures(lastPictures);
+  const igMediaFields = ['id', 'media_url', 'caption', 'permalink'];
 
-      const { username } = await InstagramClient.getSelfUser();
-      setIgLink(`https://www.instagram.com/${username}`);
-
-      setIsLoading(false);
-    };
-
-    fetchIgData();
-  }, []);
+  const { data: igPictures, isLoading } = useQuery('igLastPosts', () =>
+    InstagramClient.getLastPictures(3, igMediaFields)
+  );
 
   return (
     <ContainerInstagram>
@@ -37,29 +31,34 @@ const Instagram = () => {
         Acompanhe-nos no <Highlight>Instagram</Highlight>
       </InstagramText>
 
-      {/* TODO: Loading Skeleton */}
       {isLoading ? <></> : <></>}
 
       <InstagramItemsContainer>
-        {igPictures.map((pic) => (
-          <a key={pic.id} href={pic.permalink} target="_blank" rel="noreferrer">
-            <IgPictureCard style={{ maxWidth: '320px' }}>
-              <OutlinedImage
-                src={pic.media_url}
-                imgBorderRadius="8px"
-                rightOffset="-8px"
-                topOffset="-8px"
-                bottomOffset="8px"
-                leftOffset="8px"
-              />
-              <p>{pic.caption}</p>
-            </IgPictureCard>
-          </a>
-        ))}
+        {igPictures &&
+          igPictures.map((pic) => (
+            <a
+              key={pic.id}
+              href={pic.permalink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IgPictureCard style={{ maxWidth: '320px' }}>
+                <OutlinedImage
+                  src={pic.media_url}
+                  imgBorderRadius="8px"
+                  rightOffset="-8px"
+                  topOffset="-8px"
+                  bottomOffset="8px"
+                  leftOffset="8px"
+                />
+                <p>{pic.caption}</p>
+              </IgPictureCard>
+            </a>
+          ))}
       </InstagramItemsContainer>
 
       <a
-        href={igLink}
+        href={`https://www.instagram.com/${user.username}`}
         target="_blank"
         rel="noreferrer"
         style={{ alignSelf: 'flex-end' }}
