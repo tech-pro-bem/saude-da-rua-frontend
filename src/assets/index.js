a95b5ec41909afca4cfe38b0066fc5c8a11b97db
Maridh <maribranding@gmail.com>
2022-03-29 21:02:16 -0300

    Remove images

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,11 +7,7 @@ import action2021 from './action2021.png';
 import action2020 from './action2020.png';
 import humanDay from './humanDay.png';
 import slideArrow from './arrow.png';
-import ellipse1 from './ellipse1.png';
-import ellipse2 from './ellipse2.png';
 import banner from './voluntary.png';
-import EllipseBco from './EllipseBco.png';
-import EllipseBlue from './EllipseBlue.png';
 import Arrow from './arrow.png';
 
 export {
@@ -23,11 +19,7 @@ export {
   action2020,
   humanDay,
   slideArrow,
-  ellipse1,
-  ellipse2,
   mapImag,
   banner,
-  EllipseBco,
-  EllipseBlue,
   Arrow,
 };
