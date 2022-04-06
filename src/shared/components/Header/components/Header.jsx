807b5958235a8685ea9e4fa935e3498094d37753
Lucas <lucas-m@outlook.com>
2022-04-06 20:35:01 -0300

    feat: add link to logo

--- src/shared/components/Header/components/Header.jsx
+++ src/shared/components/Header/components/Header.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../../../assets/Logo_saude_da_rua.png';
 import { Container } from './Header.style.js';
 
-// TODO
-// - padding é palavra reservada, refatorar
-// - não há motivos para ter três DIVs encadeadas aqui
-
 const Header = () => (
   <Container>
-    <img src={logo} alt="Logo Saúde da Rua" className="logo" />
+    <Link to="/">
+      <img src={logo} alt="Logo Saúde da Rua" className="logo" />
+    </Link>
   </Container>
 );
 
