9a14736a8a15bbc90d8975e9d80a9b995c23fb8b
Lucas <lucas-m@outlook.com>
2022-04-06 20:40:12 -0300

    fix: header height

--- src/shared/components/Header/components/Header.style.js
+++ src/shared/components/Header/components/Header.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding-left: 8.375rem;
-  margin-top: 1.0625rem;
-  margin-bottom: 1.0625rem;
+  height: 93px;
+  display: flex;
+  align-items: center;
 `;
