012e14452b0bfd6ad7c7e3996a6fdb00f05f9e0e
Lucas <lucas-m@outlook.com>
2022-04-06 21:26:37 -0300

    fix: add links and donation button to header

--- src/shared/components/Header/components/Header.jsx
+++ src/shared/components/Header/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../assets/Logo_saude_da_rua.png';
-import { Container } from './Header.style.js';
+import { Container, DonationButton, LinksContainer } from './Header.style.js';
 
 const Header = () => (
   <Container>
     <Link to="/">
       <img src={logo} alt="Logo Saúde da Rua" className="logo" />
     </Link>
+    <LinksContainer>
+      <Link to="doacao">Doações</Link>
+    </LinksContainer>
+    <DonationButton text="Doe agora" route="/doacao" />
   </Container>
 );
 
