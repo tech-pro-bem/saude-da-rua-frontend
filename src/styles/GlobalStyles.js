968f19159663b83a495a49fdf711e32e11964e5e
priscila andreani <priscilaandreani@gmail.com>
2022-04-13 20:16:54 -0300

    fix: globalStyles: remove line-height: auto

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -17,41 +17,35 @@ const GlobalStyle = createGlobalStyle`
 
   p.big {
     font-weight: 400;
-    line-height: auto;
     font-size: 1.375rem;
   }
 
 
   p {
     font-weight: 400;
-    line-height: auto;
     font-size: 1rem;
     color: ${(props) => props.theme.colors.primary_light};
   }
 
   h1 {
     font-weight: 700;
-    line-height: auto;
     font-size: 3.25rem;
     color: ${(props) => props.theme.colors.primary_light};
   }
 
   h2 {
     font-weight: 600;
-    line-height: auto;
     font-size: 2.5rem;
     color: ${(props) => props.theme.colors.primary_light};
   }
 
   h3 {
     font-weight: 500;
-    line-height: auto;
     font-size: 1.75rem;
   }
 
   h4 {
     font-weight: 400;
-    line-height: auto;
     font-size: 1rem;
     color: ${(props) => props.theme.colors.primary};
   }
@@ -60,7 +54,6 @@ const GlobalStyle = createGlobalStyle`
     border: none;
     outline: none;
     cursor: pointer;
-    line-height: auto;
     font-size: 1rem;
     font-weight: 400;
   }
@@ -69,7 +62,6 @@ const GlobalStyle = createGlobalStyle`
     border: none;
     outline: none;
     cursor: pointer;
-    line-height: auto;
     font-size: 1rem;
     font-weight: 400;
 
