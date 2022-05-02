ea359aca4556cbb9b6fe2f6d662893a843e4ece4
Lucas <lucas-m@outlook.com>
2022-05-02 09:55:12 -0300

    feat: Volunteers Page: add headline content

--- src/pages/Volunteers/Volunteers.jsx
+++ src/pages/Volunteers/Volunteers.jsx
@@ -1,7 +1,16 @@
 import { Structure } from '../../shared/components';
 
 const Volunteers = () => {
-  return <Structure></Structure>;
+  return (
+    <Structure>
+      <h1>Seja uma pessoa Voluntária</h1>
+      <p>
+        Se você é profissional ou estudante da área da saúde, participe das
+        ações do Saúde da Rua e ajude-nos a levar qualidade de vida e dignidade
+        a todos! Inscreva-se agora e em breve entraremos em contato.
+      </p>
+    </Structure>
+  );
 };
 
 export default Volunteers;
