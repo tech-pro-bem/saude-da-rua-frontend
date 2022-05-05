61292c2dadbeba43480bdc4fa72162933ec0645f
leandrosax10 <leandrosantosti730@gmail.com>
2022-05-04 23:06:44 -0300

    TPB30 - Alter component style reusable

--- src/pages/Volunteers/Volunteers.jsx
+++ src/pages/Volunteers/Volunteers.jsx
@@ -1,14 +1,11 @@
 import { Structure } from '../../shared/components';
 import { Headline } from './components';
-import InputDate from '../../shared/components/InputDate/InputDate';
 
 const Volunteers = () => {
   return (
     <>
       <Headline />
-      <Structure>
-        <InputDate id="inputBirthday" registerValue="dataNacimento" />
-      </Structure>
+      <Structure></Structure>
     </>
   );
 };
--- src/shared/components/InputDate/InputDate.style.js
+++ src/shared/components/InputDate/InputDate.style.js
@@ -7,4 +7,17 @@ export const InputDateStyle = styled.input`
 
   width: 270px;
   height: 45px;
+
+  border: 1px solid;
+  border-color: ${(props) => props.theme.colors.border_input};
+  border-radius: 5px;
+  box-sizing: border-box;
+  font-size: 18px;
+  color: ${(props) => props.theme.colors.primary_light};
+  background-color: transparent;
+
+  ::placeholder {
+    color: ${(props) => props.theme.colors.primary_light};
+    font-size: 18px;
+  }
 `;
