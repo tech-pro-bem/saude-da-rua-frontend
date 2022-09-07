fb971e34b58ec11b9568fedc2d48ecb888b37173
Amanda Fernandes <aferanda@gmail.com>
2022-09-07 20:43:47 -0300

    style: add style to highlight

--- src/components/Highlight/Highlight.jsx
+++ src/components/Highlight/Highlight.jsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 const Highlight = styled.span`
   color: ${(props) => props.theme.colors.primary};
+
+  @media screen and (max-width: 520px) {
+    text-decoration: none !important;
+    font-weight: 700;
+  }
 `;
 
 export default Highlight;
