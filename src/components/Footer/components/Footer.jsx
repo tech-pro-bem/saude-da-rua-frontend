5ee0efc9d27add38a1b70a290cbf5c21bc6caad9
Isaque <isaque.error404@gmail.com>
2022-09-11 10:35:18 -0300

    feat: add news featuresand news components

--- src/components/Footer/components/Footer.jsx
+++ src/components/Footer/components/Footer.jsx
@@ -1,82 +1,138 @@
 import React from 'react';
 
-import { Contacts } from './Footer.style.js';
+import {
+  Contact,
+  Copyright,
+  CopyrightMobile,
+  Divider,
+  FooterContainer,
+  HeadersFooter,
+  Locations,
+  Logo,
+  LogoMobile,
+  MainFooter,
+  SocialMedia,
+  SocialMediaMobile,
+  SocialMediaMobileContainer,
+  Text,
+  Title,
+} from './Footer.style.js';
 
-import { social1, social2, social3 } from '@assets';
+import { Email, Instagram, WhatsApp } from '@assets';
 
 const Footer = () => (
-  <Contacts>
-    <hr />
-    <div className="ft-main">
-      <div className="ft-main-item-one">
-        <h2 className="ft-titleOne">Saúde da Rua</h2>
-        <p>
-          O Saúde da Rua nasceu em julho de 2020 com a missão
-          <br /> de
-          <strong> levar atendimento médico humanizado e recorrente </strong>
+  <FooterContainer>
+    <Divider />
+    <MainFooter>
+      <HeadersFooter>
+        <Title>Saúde da Rua</Title>
+        <Text>
+          Criado por estudantes de medicina, o Saúde da Rua
           <br />
-          às camadas mais vulneráveis da sociedade.
-        </p>
-      </div>
+          leva <strong>atendimento médico humanizado e recorrente</strong>{' '}
+          <br />
+          às populações mais vulneráveis da sociedade.
+          <br />O Saúde da Rua nasceu em julho de 2020 com a missão
+        </Text>
+      </HeadersFooter>
 
-      <div className="ft-main-item-two">
-        <h2 className="ft-titleTwo">Onde estamos</h2>
-        <ul className="locations">
+      <Locations>
+        <Title>Onde estamos</Title>
+        <ul>
           <li>São Paulo - SP</li>
           <li>Campinas - SP</li>
           <li>São José do Rio Preto - SP</li>
           <li>São Carlos - SP</li>
           <li>Itajaí - SC</li>
         </ul>
-      </div>
+      </Locations>
 
-      <div className="ft-main-item-three">
-        <h2 className="ft-titleThree">Contato</h2>
-        <ul className="midia">
+      <Contact>
+        <Title>Contato</Title>
+        <SocialMedia>
           <li>
             <a
               href="https://wa.me/5511985914817"
               target="_blank"
               rel="noreferrer noopener"
             >
-              <img src={social1} alt="WhatsApp" className="logo" />
+              <Logo src={WhatsApp} alt="WhatsApp" />
               WhatsApp
             </a>
           </li>
           <li>
             <a href="mailto:saudedarua@gmail.com">
-              <img src={social2} alt="E-mail" className="logo" />
+              <Logo src={Email} alt="E-mail" />
               saudedarua@gmail.com
             </a>
           </li>
+
           <li>
             <a
-              href="https://instagram.com/saudedarua?utm_medium=copy_link"
+              href="https://wa.me/5511985914817"
               target="_blank"
               rel="noreferrer noopener"
             >
-              <img src={social3} alt="Instagram" className="logo" />
-              @saudedarua
+              <Logo src={Instagram} alt="Instagram" />
+              Instagram
             </a>
           </li>
-        </ul>
-      </div>
-    </div>
+        </SocialMedia>
+      </Contact>
 
-    <div className="ft-legal">
-      <ul className="ft-legal-list">
-        <li>Copyright©2021, Saúde da Rua. Todos os direitos reservados</li>
-        <li>|</li>
-        <li>
-          <strong>Política de privacidade</strong>
-        </li>
-        <li>|</li>
-        <li>
-          Feito com amor pela <strong>Tech Pro Bem </strong>
-        </li>
-      </ul>
-    </div>
-  </Contacts>
+      <SocialMediaMobileContainer>
+        <Title>Contato</Title>
+        <SocialMediaMobile>
+          <a
+            href="https://wa.me/5511985914817"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <LogoMobile src={WhatsApp} alt="WhatsApp" />
+          </a>
+
+          <a href="mailto:saudedarua@gmail.com">
+            <LogoMobile src={Email} alt="E-mail" />
+          </a>
+
+          <a
+            href="https://instagram.com/saudedarua?utm_medium=copy_link"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <LogoMobile src={Instagram} alt="Instagram" />
+          </a>
+        </SocialMediaMobile>
+      </SocialMediaMobileContainer>
+    </MainFooter>
+
+    <Copyright>
+      <li>Copyright©2021, Saúde da Rua. Todos os direitos reservados</li>
+      <li>|</li>
+      <li>
+        <strong>Política de privacidade</strong>
+      </li>
+      <li>|</li>
+
+      <li>
+        Feito com amor pela <strong>Tech Pro Bem </strong>
+      </li>
+    </Copyright>
+
+    <CopyrightMobile>
+      <li>
+        <strong>Política de privacidade</strong>
+      </li>
+      <li>
+        Feito com amor pela <strong>Tech Pro Bem </strong>
+      </li>
+      <li>
+        Copyright©2021, Saúde da Rua.
+        <br />
+        Todos os direitos reservados
+      </li>
+    </CopyrightMobile>
+  </FooterContainer>
 );
 
 export default Footer;
