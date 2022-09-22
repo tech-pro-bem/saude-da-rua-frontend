dfd44a0b1135851f0e67eabad98e368b54b12ad4
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-22 16:14:33 -0300

    refactor: rename socials logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,17 +1,17 @@
+import action2020 from './action2020.png';
+import action2021 from './action2021.png';
+import Arrow from './arrow.png';
+import banner from './banner-home.png';
+import Email from './Email.svg';
+import humanDay from './humanDay.png';
+import Instagram from './Instagram.svg';
+import Logo from './Logo_saude_da_rua.png';
+import mapImag from './mapa.png';
+import partnersLogo from './partners.png';
 import imag1 from './volunter1.png';
 import imag2 from './volunter2.png';
 import imag3 from './volunter3.png';
-import mapImag from './mapa.png';
-import partnersLogo from './partners.png';
-import action2021 from './action2021.png';
-import action2020 from './action2020.png';
-import humanDay from './humanDay.png';
-import Arrow from './arrow.png';
-import banner from './banner-home.png';
-import Logo from './Logo_saude_da_rua.png';
-import social1 from './WhatsApp.svg';
-import social2 from './Email.svg';
-import social3 from './Instagram.svg';
+import WhatsApp from './WhatsApp.svg';
 
 export {
   imag1,
@@ -25,7 +25,7 @@ export {
   Arrow,
   banner,
   Logo,
-  social1,
-  social2,
-  social3,
+  WhatsApp,
+  Email,
+  Instagram,
 };
