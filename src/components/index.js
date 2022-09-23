053f103f19f64552986e051a2d9bf1f1cdfb7561
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-23 10:01:05 -0300

    refactor: removed import components in folder header and footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,13 @@
-import Structure from './Structure/Structure';
-import Header from './Header/components/Header';
 import { Button } from './Button/Button';
-import { LinkButton } from './LinkButton/LinkButton.jsx';
-import Footer from './Footer/components/Footer';
-import Layout from './Layout/Layout';
-import Highlight from './Highlight/Highlight';
 import Chip from './Chip/Chip';
-import Quotation from './Quotation/Quotation';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Highlight from './Highlight/Highlight';
+import Layout from './Layout/Layout';
+import { LinkButton } from './LinkButton/LinkButton.jsx';
 import OutlinedImage from './OutlinedImage/OutlinedImage';
+import Quotation from './Quotation/Quotation';
+import Structure from './Structure/Structure';
 
 export {
   Layout,
