8db0ffa9a0f27bd4d72fcbe043e979c9f3e7d71a
Amanda Fernandes <aferanda@gmail.com>
2022-09-24 14:40:57 -0300

    style: remove unused code

--- src/pages/Home/containers/Partners/Partners.style.js
+++ src/pages/Home/containers/Partners/Partners.style.js
@@ -1,25 +1,5 @@
 import styled from 'styled-components';
 
-export const PartnersText = styled.h3`
-  display: block;
-  width: 50%;
-  height: 42px;
-  margin-left: -200px;
-  margin-top: auto;
-  margin-bottom: 64px;
-  color: ${(props) => props.theme.colors.primary_light};
-
-  span {
-    display: block;
-    width: 50%;
-    height: 42px;
-    margin-left: 270px;
-    margin-top: -42px;
-    margin-bottom: 20px;
-    color: ${(props) => props.theme.colors.primary};
-  }
-`;
-
 export const ContainerPartners = styled.div`
   margin-top: 100px;
   margin-bottom: 100px;
