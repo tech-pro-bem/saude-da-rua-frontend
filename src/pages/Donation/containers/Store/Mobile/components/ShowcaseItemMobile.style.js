4e1b1a277a2a0c160c8480b04f216a5b8e12bd06
Laura Neves <lauracmneves@gmail.com>
2022-09-27 15:45:42 -0300

    feat: create the showCaseItem for mobile

---
+++ src/pages/Donation/containers/Store/Mobile/components/ShowcaseItemMobile.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ShowcaseItemContainer = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  p {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    text-align: center;
+    margin-top: 9px;
+  }
+`;
