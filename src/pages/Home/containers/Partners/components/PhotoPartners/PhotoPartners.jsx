151b7cd18bb9f84ac3a195dc25b585170e7735bc
Amanda Fernandes <aferanda@gmail.com>
2022-09-28 20:52:44 -0300

    feat: add carousel to partners

--- src/pages/Home/containers/Partners/components/PhotoPartners/PhotoPartners.jsx
+++ src/pages/Home/containers/Partners/components/PhotoPartners/PhotoPartners.jsx
@@ -1,9 +1,59 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import { partnersLogo } from '@assets';
-import { PartnersPhoto } from './PhotoPartners.style';
+
+import { ContainerPhotoPartners, PartnersPhoto } from './PhotoPartners.style';
+
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+import { Grid, Pagination } from 'swiper';
+
+// adicionar os futuros parceiros nesse array.
+const partners = [
+  partnersLogo,
+  // partnersLogo,
+  // partnersLogo,
+  // partnersLogo,
+  // partnersLogo,
+  // partnersLogo,
+  // partnersLogo,
+];
 
 const PhotoPartners = () => {
   return (
-    <PartnersPhoto src={partnersLogo} alt="Nossos parceiros: MD e Spirit" />
+    <>
+      {partners.length > 1 ? (
+        <ContainerPhotoPartners>
+          <Swiper
+            slidesPerView={1}
+            grid={{
+              rows: 2,
+            }}
+            spaceBetween={24}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Grid, Pagination]}
+            className="mySwiper"
+          >
+            {partners.map((partner, idx) => (
+              <SwiperSlide key={idx}>
+                <img src={partner} alt="logo da empresa parceira" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </ContainerPhotoPartners>
+      ) : (
+        <>
+          <PartnersPhoto
+            src={partnersLogo}
+            alt="Nossos parceiros: MD e Spirit"
+          />
+        </>
+      )}
+    </>
   );
 };
 
