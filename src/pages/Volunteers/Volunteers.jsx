bc0d911ffd82a8b19802f5a4695aa979d0baee2f
Laura Neves <lauracmneves@gmail.com>
2022-10-05 21:07:46 -0300

    chore: deletion of the headline import

--- src/pages/Volunteers/Volunteers.jsx
+++ src/pages/Volunteers/Volunteers.jsx
@@ -1,18 +1,17 @@
 import { Structure } from '@components';
-import { Headline, WhoCanVolunteer, Banner } from './containers';
+import { WhoCanVolunteer, Banner } from './containers';
 import { Head } from '@hooks';
 import { VolunteersContainer } from './containers/Volunteers.style';
-
 
 const Volunteers = () => {
   return (
     <>
-      <Banner />  
+      <Banner />
       <Head
         title="Seja voluntario"
         description="Se você é profissional ou estudante da área da saúde, participe das ações do Saúde da Rua"
       />
-      <VolunteersContainer>        
+      <VolunteersContainer>
         <Structure>
           <WhoCanVolunteer />
         </Structure>
