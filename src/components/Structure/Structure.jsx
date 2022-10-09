11fb59cbba18868240d9d4dac6488087bedff32d
Amanda Fernandes <aferanda@gmail.com>
2022-10-09 15:13:22 -0300

    style: config padding to bigger screens

--- src/components/Structure/Structure.jsx
+++ src/components/Structure/Structure.jsx
@@ -5,6 +5,10 @@ const Structure = styled.div`
   max-width: 100%;
   margin: 0 auto;
   padding: 0 15px;
+
+  @media screen and (min-width: 640px) {
+    padding: 0 30px;
+  }
 `;
 
 export default Structure;
