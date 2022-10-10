06ddb29c82656740d94784e388d7952b89997c73
isaque.s <isaque.error404@gmail.com>
2022-10-10 15:28:41 -0300

    feat: export new image

--- src/assets/volunteerPage/index.js
+++ src/assets/volunteerPage/index.js
@@ -1,7 +1,15 @@
-import experience from './experience.png';
 import experience2 from './experience-2.png';
 import experience3 from './experience-3.png';
 import experience4 from './experience-4.png';
 import experience5 from './experience-5.png';
+import experience from './experience.png';
+import howwilltheexpirence from './howwilltheexpirence.png';
 
-export { experience, experience2, experience3, experience4, experience5 };
+export {
+  experience,
+  experience2,
+  experience3,
+  experience4,
+  experience5,
+  howwilltheexpirence,
+};
