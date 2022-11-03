47b19fad04c73f1ddd399cadfa28a889b1299e1c
Amanda Fernandes <aferanda@gmail.com>
2022-11-03 13:01:07 -0300

    fix: skip line and change the color of the question mark

--- src/pages/Volunteers/containers/WhatDoWeExpect/Desktop/WhatDoWeExpectDesktop.jsx
+++ src/pages/Volunteers/containers/WhatDoWeExpect/Desktop/WhatDoWeExpectDesktop.jsx
@@ -5,8 +5,9 @@ const values = [
   {
     id: 1,
     trigger: 'Comprometimento',
-    content:
-      'Trabalho voluntário deve ser levado a sério! Por isso, pontualidade e comunicação são cruciais nessa jornada.',
+    content: 'Trabalho voluntário deve ser levado a sério!',
+    content2:
+      'Por isso, pontualidade e comunicação são cruciais nessa jornada.',
   },
   {
     id: 2,
@@ -44,13 +45,14 @@ const WhatDoWeExpectDesktop = () => {
   return (
     <Container>
       <h3>
-        O que esperamos de <Highlight>você?</Highlight>
+        O que esperamos de <Highlight>você</Highlight>?
       </h3>
       <GridContent>
         {values.map((item) => (
           <div key={item.id}>
             <h4>{item.trigger}</h4>
             <p>{item.content}</p>
+            <p>{item.content2}</p>
           </div>
         ))}
       </GridContent>
