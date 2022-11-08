64d04bcac00e98819cf084217591d94be1ae685e
Amanda Fernandes <aferanda@gmail.com>
2022-11-08 15:41:48 -0300

    style: create style to Headline

---
+++ src/pages/SignUp/containers/Headline/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 40px;
+
+  h1 {
+    font-size: 2rem;
+    line-height: 3rem;
+    margin-bottom: 1.5rem;
+  }
+
+  p {
+    font-size: 1.125rem;
+    line-height: 1, 6875rem;
+    margin-bottom: 1.6rem;
+  }
+`;
