270816c3e91d129a58a2f498ec145f3bbb194a31
Amanda Fernandes <aferanda@gmail.com>
2022-11-08 15:43:56 -0300

    feat: add sign up page and update route

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,15 @@
+import { Structure } from '@components';
+
+import { ContactInfo, Headline } from './containers';
+
+const SignUp = () => {
+  return (
+    <Structure>
+      <Headline />
+
+      <ContactInfo />
+    </Structure>
+  );
+};
+
+export default SignUp;
