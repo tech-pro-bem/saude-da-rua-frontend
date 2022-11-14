a1ce7bcf2d8b62311990d992526183a39215a7b5
Amanda Fernandes <aferanda@gmail.com>
2022-11-14 09:50:09 -0300

    feat: add agree component

---
+++ src/pages/SignUp/containers/Agree/index.jsx
@@ -0,0 +1,15 @@
+import { Container } from './style';
+import { Highlight } from '@components';
+
+export const Agree = ({ register }) => {
+  return (
+    <Container>
+      <input id="agree" type="checkbox" {...register('agree')} />
+      <label htmlFor="agree">
+        Ao encaminhar as suas informações, você declara que leu e está de acordo
+        com o tratamento dos seus dados pelo Saúde da Rua conforme estabelecido
+        na nossa <Highlight>Política de Privacidade</Highlight>.
+      </label>
+    </Container>
+  );
+};
