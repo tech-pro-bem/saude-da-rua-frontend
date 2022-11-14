d5717ddcf9aab2a9f93fa69c0c8e7bf20f9af607
Amanda Fernandes <aferanda@gmail.com>
2022-11-14 09:51:47 -0300

    feat: add components to SignUp page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,20 +1,40 @@
 import { Structure } from '@components';
 import { useForm, Controller } from 'react-hook-form';
 
-import { ContactInfo, Headline } from './containers';
+import {
+  ContactInfo,
+  Headline,
+  Availability,
+  Agree,
+  HowDidYouFindUs,
+} from './containers';
+import { SubmitStyle } from './style';
 
 const SignUp = () => {
-  const { register, control } = useForm();
+  const { register, control, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <Structure>
       <Headline />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ContactInfo
+          register={register}
+          Controller={Controller}
+          control={control}
+        />
 
-      <ContactInfo
-        register={register}
-        Controller={Controller}
-        control={control}
-      />
+        <Availability register={register} />
+
+        <HowDidYouFindUs register={register} />
+
+        <Agree register={register} />
+
+        <SubmitStyle type="submit" value="Finalizar meu cadastro" />
+      </form>
     </Structure>
   );
 };
