000f70e62ea98a54f812ba74af381431f69fee6e
Amanda Fernandes <aferanda@gmail.com>
2022-11-14 09:49:13 -0300

    feat: add availability component

---
+++ src/pages/SignUp/containers/Availability/index.jsx
@@ -0,0 +1,42 @@
+import { Container, InputsBox, Step } from './style';
+
+const daysOfWeek = [
+  { id: 1, name: 'Segunda-feira', value: 'monday' },
+  { id: 2, name: 'Terça-feira', value: 'tuesday' },
+  { id: 3, name: 'Quarta-feira', value: 'wednesday' },
+  { id: 4, name: 'Quinta-feira', value: 'thursday' },
+  { id: 5, name: 'Sexta-feira', value: 'friday' },
+  { id: 6, name: 'Sábado', value: 'saturday' },
+  { id: 7, name: 'Domingo', value: 'sunday' },
+];
+
+export const Availability = ({ register }) => {
+  return (
+    <Container>
+      <Step>
+        <span>Etapa 3 de 3</span>
+        <h2>Disponibilidade</h2>
+      </Step>
+
+      <p>
+        Informe em quais dias da semana, nos próximos 4 meses, você poderá
+        integrar nossas ações:
+      </p>
+
+      <InputsBox>
+        {daysOfWeek.map((day) => (
+          <div key={day.id}>
+            <input
+              type="checkbox"
+              id={day.value}
+              name={day.value}
+              value={day.value}
+              {...register('available')}
+            />
+            <label htmlFor={day.value}>{day.name}</label>
+          </div>
+        ))}
+      </InputsBox>
+    </Container>
+  );
+};
