1ac58dcbf887663c0398ca72484603eb0f730e64
isaque.s <isaque.error404@gmail.com>
2022-11-17 12:44:31 -0300

    feat: export WhatToContinueContributed mobile e desktop

---
+++ src/pages/Medicament/containers/index.js
@@ -0,0 +1,4 @@
+import WhatToContinueContributedDesktop from './WantToContinueContributed/Desktop/WantToContinueContributedDesktop';
+import WhatToContinueContributedMobile from './WantToContinueContributed/Mobile/WantToContinueContributedMobile';
+
+export { WhatToContinueContributedMobile, WhatToContinueContributedDesktop };
