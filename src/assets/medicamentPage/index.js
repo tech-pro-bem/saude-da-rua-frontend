4d76a86f78c11671931b613348c9745dd319ddde
isaque.s <isaque.error404@gmail.com>
2022-11-17 12:36:27 -0300

    feat: export image

---
+++ src/assets/medicamentPage/index.js
@@ -0,0 +1,11 @@
+import WantToContinueContributed2 from './want-to-continue-contributed-2.png';
+
+import WantToContinueContributed3 from './want-to-continue-contributed-3.png';
+
+import WantToContinueContributed from './want-to-continue-contributed.png';
+
+export {
+  WantToContinueContributed3,
+  WantToContinueContributed2,
+  WantToContinueContributed,
+};
