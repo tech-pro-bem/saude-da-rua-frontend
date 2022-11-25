8ba59416e41b822e18234c69d16b0787b3d2582e
Amanda Fernandes <aferanda@gmail.com>
2022-11-25 16:15:52 -0300

    style: add styles to privacy policy page

---
+++ src/pages/PrivacyPolicy/style.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+import { Structure } from '@components';
+
+export const Container = styled(Structure)`
+  padding-top: 46.57px;
+  padding-bottom: 40px;
+
+  @media (min-width: 768px) {
+    padding-top: 173px;
+  }
+
+  h1,
+  h2,
+  h3,
+  p,
+  ul,
+  ol {
+    color: #fbfbfb;
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 48px;
+
+    @media (min-width: 768px) {
+      font-weight: 700;
+      font-size: 52px;
+      line-height: 78px;
+      text-align: center;
+      margin-bottom: 24px;
+    }
+  }
+
+  h2,
+  h3 {
+    font-size: 22px;
+    line-height: 33px;
+    font-weight: 600;
+    margin-bottom: 24px;
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 27px;
+    margin-bottom: 24px;
+  }
+
+  ul,
+  ol {
+    font-size: 18px;
+    list-style-position: inside;
+  }
+
+  ul {
+    margin-bottom: 24px;
+  }
+
+  ol {
+    list-style: none;
+
+    li {
+      margin-bottom: 24px;
+
+      &:last-of-type {
+        margin-bottom: 0;
+        padding-bottom: 24px;
+      }
+    }
+  }
+
+  a {
+    color: ${(props) => props.theme.colors.light};
+    font-weight: 500;
+    cursor: pointer;
+
+    &:hover {
+      font-weight: 600;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  @media (min-width: 768px) {
+    columns: 2;
+    column-gap: 2rem;
+  }
+
+  @media (min-width: 1024px) {
+    height: 2950px;
+    column-fill: auto;
+  }
+`;
