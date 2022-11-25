573e966d44da2532869404559f64e3550f53b02e
Amanda Fernandes <aferanda@gmail.com>
2022-11-25 16:14:49 -0300

    feat: add containers to privacy policy page

---
+++ src/pages/PrivacyPolicy/containers/ForWhatPurposesDoWeCollectTheData/index.jsx
@@ -0,0 +1,12 @@
+export const ForWhatPurposesDoWeCollectTheData = () => {
+  return (
+    <div>
+      <h2>Seção 4 - Para quais finalidades coletamos os dados?</h2>
+      <p>
+        Os dados fornecidos por nossos usuários são coletados com a única
+        finalidade de analisar os perfis das pessoas interessadas e entrar em
+        contato de acordo com a necessidade do <a href="/">Saúde na Rua.</a>
+      </p>
+    </div>
+  );
+};
---
+++ src/pages/PrivacyPolicy/containers/Headline/index.jsx
@@ -0,0 +1,10 @@
+export const Headline = () => {
+  return (
+    <div>
+      <h1>Política de privacidade</h1>
+      <p>
+        Este site é mantido e operado por <a href="/">Saúde na Rua.</a>
+      </p>
+    </div>
+  );
+};
