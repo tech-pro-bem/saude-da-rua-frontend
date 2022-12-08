84b5d27b8c6566278ea21f69783b3e12cafd18bc
Laura Neves <lauracmneves@gmail.com>
2022-12-07 21:49:50 -0300

    fix: axios instance url

--- src/pages/SignUpFail/SignUpFail.jsx
+++ src/pages/SignUpFail/SignUpFail.jsx
@@ -1,5 +1,4 @@
 import { Structure, LinkButton } from '@components';
-import { ConfirmIcon } from '../../assets/svg';
 import { Container, Head, TextBox } from './style';
 
 const SignUpFail = () => {
@@ -7,18 +6,11 @@ const SignUpFail = () => {
     <Structure>
       <Container>
         <Head>
-          {/* <img src={ConfirmIcon} alt="" /> */}
           <h1>Ocorreu um erro ao efetuar inscrição :( </h1>
         </Head>
         <TextBox>
           {/* <p>
-            Seu nome agora está no nosso{' '}
-            <strong>banco de pessoas voluntárias</strong>.{' '}
-            <span>
-              Cerca de 15 dias antes de cada ação, convocamos os voluntários
-              pelo Whatsapp. Enquanto aguarda o chamado, que tal nos ajudar
-              ainda mais?
-            </span>
+
           </p> */}
         </TextBox>
         <LinkButton route="/inscreva-se" text="Tente novamente" />
