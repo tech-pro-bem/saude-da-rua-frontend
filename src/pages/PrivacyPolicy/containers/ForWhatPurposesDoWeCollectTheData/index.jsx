25cafbf757738ef70ad4d18760b2db655a98ae1d
isaque.s <isaque.error404@gmail.com>
2022-12-11 11:22:18 -0300

    feat: removed paragraph

--- src/pages/PrivacyPolicy/containers/ForWhatPurposesDoWeCollectTheData/index.jsx
+++ src/pages/PrivacyPolicy/containers/ForWhatPurposesDoWeCollectTheData/index.jsx
@@ -3,11 +3,8 @@ export const ForWhatPurposesDoWeCollectTheData = () => {
     <section id="secao4">
       <h2>Seção 4 - Para quais finalidades coletamos os dados?</h2>
       <p>
-        Os dados fornecidos por nossos usuários são coletados com a única
-        finalidade de analisar os perfis das pessoas interessadas e entrar em
-        contato de acordo com a necessidade do <a href="/">Saúde na Rua.</a>
         Os dados fornecidos por nossos usuários são coletados com a
-        <strong> única finalidade</strong>
+        <strong> única finalidade </strong>
         de analisar os perfis das pessoas interessadas e entrar em contato de
         acordo com a necessidade do <a href="/">Saúde na Rua.</a>
       </p>
