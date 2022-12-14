3d9c5114e1bbc9cd443b42d385ee11e064a77d54
isaque.s <isaque.error404@gmail.com>
2022-12-14 11:34:39 -0300

    feat: import api and create new function GenerateQrCode

--- src/shared/services/GenerateQrCode/GenerateQrCode.js
+++ src/shared/services/GenerateQrCode/GenerateQrCode.js
@@ -1,3 +1,18 @@
-const { REACT_APP_IG_TOKEN, REACT_APP_IG_BASE_URL: baseURL } = process.env;
+import api from '../api';
 
-export async function GenerateQrCode() {}
+export async function GenerateQrCode() {
+  let responseData = {
+    key: '',
+    error: false,
+  };
+  try {
+    responseData.error = false;
+    const { data } = await api.get('/pix');
+    responseData.key = data.key;
+  } catch (error) {
+    responseData.error = true;
+    responseData.key = null;
+  } finally {
+    return responseData;
+  }
+}
