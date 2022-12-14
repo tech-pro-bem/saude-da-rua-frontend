64450728b69ff8fc4514f6bd4ff841ec5a39ea48
isaque.s <isaque.error404@gmail.com>
2022-12-14 11:29:17 -0300

    feat: export img

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,18 +1,19 @@
 import assistanceIcon from './assistance.svg';
-import healthcareIcon from './healthcare.svg';
-import supportIcon from './support.svg';
-import healthPeople from './Helth.svg';
 import Icone_Estudantes from './A_Estudantes.svg';
 import Icone_Medicos from './B_Médicos.svg';
+import CaretDown from './caret_down.svg';
+import CaretUp from './caret_up.svg';
+import ConfirmIcon from './confirm_icon.svg';
 import Icone_Enfermeiros from './C_Enfermeiros.svg';
 import Icone_Nutricionista from './D_Nutricionista.svg';
 import Icone_Farmacia from './E_Farmácia.svg';
 import Icone_Assistente_social from './F_Assistente_Social.svg';
 import Icone_veterinario from './G_Veterinaria.svg';
+import healthcareIcon from './healthcare.svg';
+import healthPeople from './healthPeople.svg';
+
 import Icone_Odontologia from './nurse.svg';
-import CaretDown from './caret_down.svg';
-import CaretUp from './caret_up.svg';
-import ConfirmIcon from './confirm_icon.svg';
+import supportIcon from './support.svg';
 
 export {
   assistanceIcon,
