234f60ecaed9e3efcf0b8b9aad9d4dccd9f00881
isaque.s <isaque.error404@gmail.com>
2022-12-14 11:34:01 -0300

    feat: add Skeleton, useQuery GenerateQrCode

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,24 +1,39 @@
 import { Structure } from '@components';
+import { GenerateQrCode } from '@shared/services';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useQuery } from 'react-query';
 import { Head, useMedia } from '../../hooks';
 import {
+  GalleryAndQuantation,
   HelpNowDesktop,
   HelpNowMobile,
   HowYourDonationHelp,
   Store,
-  GalleryAndQuantation,
 } from './containers';
 
 const Donation = () => {
   const mobile = useMedia('(max-width: 768px)');
+  const { isLoading, data } = useQuery('qrCode', async () => {
+    const { key } = await GenerateQrCode();
+    return key;
+  });
+
   return (
     <>
       <Head title="Doações" description="Doe para Saúde da Rua" />
-      {mobile ? (
-        <HelpNowMobile />
+      {isLoading ? (
+        <Skeleton height={200} />
       ) : (
-        <Structure>
-          <HelpNowDesktop />
-        </Structure>
+        <>
+          {mobile ? (
+            <HelpNowMobile data={data} />
+          ) : (
+            <Structure>
+              <HelpNowDesktop data={data} />
+            </Structure>
+          )}
+        </>
       )}
       <HowYourDonationHelp />
       <GalleryAndQuantation />
