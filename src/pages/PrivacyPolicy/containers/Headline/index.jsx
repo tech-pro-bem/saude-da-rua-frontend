18bf13f52b9d4aeedead413b2905c6cc558eeea3
isaque.s <isaque.error404@gmail.com>
2022-12-15 23:27:10 -0300

    feat: removed title

--- src/pages/PrivacyPolicy/containers/Headline/index.jsx
+++ src/pages/PrivacyPolicy/containers/Headline/index.jsx
@@ -2,9 +2,6 @@ export const Headline = () => {
   return (
     <div>
       <h1>Política de Privacidade</h1>
-      <p>
-        Este site é mantido e operado por <a href="/">Saúde na Rua.</a>
-      </p>
     </div>
   );
 };
