65d716b43cb4500fce74b5b80b54b57fb1ec1372
isaque.s <isaque.error404@gmail.com>
2022-12-15 23:15:18 -0300

    feat: new title

--- src/pages/PrivacyPolicy/containers/Headline/index.jsx
+++ src/pages/PrivacyPolicy/containers/Headline/index.jsx
@@ -1,7 +1,7 @@
 export const Headline = () => {
   return (
     <div>
-      <h1>Política de privacidade</h1>
+      <h1>Política de Privacidade</h1>
       <p>
         Este site é mantido e operado por <a href="/">Saúde na Rua.</a>
       </p>
