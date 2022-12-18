68820c18c99a8da28c136ad77590bb317a82a33d
Lucas <lucas-m@outlook.com>
2022-12-18 18:50:12 -0300

    refact(financial_reports): apply sort to reports response from api

--- src/shared/services/ReportService/ReportService.js
+++ src/shared/services/ReportService/ReportService.js
@@ -2,6 +2,13 @@ import api from '../../../services/api';
 
 export const getReports = async (limit = 5) => {
   const params = { limit };
-  const { data } = await api.get('/file/PDF', { params });
-  return data;
+  const { data: reports } = await api.get('/file/PDF', { params });
+
+  reports.sort((a, b) => {
+    const aDate = new Date(a.createdAt);
+    const bDate = new Date(b.createdAt);
+    return bDate - aDate;
+  });
+
+  return reports;
 };
